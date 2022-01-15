0a6b1f7e260cabef5081b07ad3595adcbe74f04d
Kevin-Sarges <kevinsarges08@gmail.com>
2022-01-15 19:27:10 -0300

    Add colors

---
+++ src/global/styles/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    secondary01: "#BC93FF",
+    secondary02: "#9F64FF",
+    secondary03: "#672ACA",
+    secondary04: "#6024C2",
+    secondary05: "#390096",
+
+    ligthGray: "#E5E5E5",
+    gray: "#C4C4C4",
+    white: "#FFFFFF",
+    black: "#000000",
+    green: "#039D00",
+  },
+};
