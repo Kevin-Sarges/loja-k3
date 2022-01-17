753af4bb6be2859deb08d07fa0cba0b3989b80e7
Kevin-Sarges <kevinsarges08@gmail.com>
2022-01-17 11:27:07 -0300

    criando fake data

---
+++ src/utils/fakeData.js
@@ -0,0 +1,58 @@
+export const productsFake = [
+  {
+    id: "1",
+    name: "Sapato",
+    category: "1",
+    price: "R$20,00",
+    whatsapp: "99999999999",
+    description:
+      "xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx",
+  },
+
+  {
+    id: "2",
+    name: "Sapato",
+    category: "2",
+    price: "R$20,00",
+    whatsapp: "99999999999",
+    description:
+      "xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx",
+  },
+  {
+    id: "3",
+    name: "Sapato",
+    category: "3",
+    price: "R$20,00",
+    whatsapp: "99999999999",
+    description:
+      "xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx",
+  },
+  {
+    id: "4",
+    name: "Sapato",
+    category: "3",
+    price: "R$20,00",
+    whatsapp: "99999999999",
+    description:
+      "xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx",
+  },
+  {
+    id: "5",
+    name: "Sapato",
+    category: "4",
+    price: "R$20,00",
+    whatsapp: "99999999999",
+    description:
+      "xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx",
+  },
+
+  {
+    id: "6",
+    name: "Sapato",
+    category: "1",
+    price: "R$20,00",
+    whatsapp: "99999999999",
+    description:
+      "xxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx",
+  },
+];
