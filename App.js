b1e4b0217d22ac0bdd98d5756ca15255005a5647
Kevin-Sarges <kevinsarges08@gmail.com>
2022-01-17 11:24:45 -0300

    Add componente de Header

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 
+import { Header } from "./src/components/Header";
 import { Routes } from "./src/routes";
+
+import Logo from "./src/assets/logo.png";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,6 +22,7 @@ export default function App() {
   return (
     <>
       <StatusBar />
+      <Header title="Loja K3" icon={Logo} />
       <Routes />
     </>
   );
