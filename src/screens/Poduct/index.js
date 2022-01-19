06b248ff3d13a339318ae2aecd79c277a46de5a0
Kevin-Sarges <kevinsarges08@gmail.com>
2022-01-19 11:25:07 -0300

    Finalizando screen de Produto

--- src/screens/Poduct/index.js
+++ src/screens/Poduct/index.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity, ScrollView, Image, View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useNavigation } from "@react-navigation/native";
+import { useRoute } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
 import { Header } from "../../components/Header";
 import { Label } from "../../components/Label";
 import { Button } from "../../components/Button";
 
 import { theme } from "../../global/styles/theme";
-import { productsFake } from "../../utils/fakeData";
 import { styles } from "./styles";
 
 export function Product() {
+  const route = useRoute();
   const navigation = useNavigation();
+
   const { secondary05, secondary02 } = theme.color;
+  const { product } = route.params;
 
   function handleGoBack() {
     navigation.goBack();
@@ -28,32 +31,28 @@ export function Product() {
         </TouchableOpacity>
       </Header>
 
-      <ScrollView style={styles.container}>
-        {productsFake.map((item) => (
-          <View key={item.id}>
-            <LinearGradient
-              style={styles.backgroundImage}
-              colors={[secondary05, secondary02]}
-            >
-              <Image source={item.image} style={styles.image} />
-            </LinearGradient>
-            <Label content={item.name} />
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+        <LinearGradient
+          style={styles.backgroundImage}
+          colors={[secondary05, secondary02]}
+        >
+          <Image source={product.image} style={styles.image} />
+        </LinearGradient>
+        <Label content={product.name} />
 
-            <View style={styles.description}>
-              <Label content="Descrição" />
-              <Text style={styles.textDescription}>{item.description}</Text>
-            </View>
+        <View style={styles.description}>
+          <Label content="Descrição" />
+          <Text style={styles.textDescription}>{product.description}</Text>
+        </View>
 
-            <View>
-              <Label content="Valor" />
-              <Text style={styles.value}>{item.price}</Text>
-            </View>
+        <View>
+          <Label content="Valor" />
+          <Text style={styles.value}>{product.price}</Text>
+        </View>
 
-            <View style={styles.footer}>
-              <Button />
-            </View>
-          </View>
-        ))}
+        <View style={styles.footer}>
+          <Button />
+        </View>
       </ScrollView>
     </>
   );
